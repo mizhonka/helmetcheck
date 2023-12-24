8578368e46a5e41fd55cce99cf32f2f3b7e89bd7
Miranda Honkanen <miranda.honkanen@helsinki.fi>
2023-12-24 20:42:55 +0200

    delete library

--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, request
+from flask import render_template, request, redirect
 from search import Search
 
 extras={ord("["):"", ord("]"):"", ord("'"):""}
@@ -17,14 +17,22 @@ def get_libraries(str_libraries):
 def add_library():
     libraries=get_libraries(request.form["libraries"])
     library=request.form["library"]
-    if library:
+    if library and library not in libraries:
         libraries.append(library)
+    return render_template("index.html", libraries=libraries)
+
+@app.route("/delete_library", methods=["POST"])
+def delete_library():
+    library=request.form["library"]
+    libraries=get_libraries(request.form["libraries"])
+    libraries.remove(library)
     return render_template("index.html", libraries=libraries)
 
 @app.route("/search", methods=["POST"])
 def search():
     libraries=get_libraries(request.form["libraries"])
-    print(libraries)
+    if not libraries:
+        return redirect("/")
     result=Search("/home/mizhonka/Documents/tbr.txt", libraries).check_availability()
     return render_template("results.html", links=result)
 
