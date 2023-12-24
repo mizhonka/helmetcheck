d33eb13e755e51bdfba922ccc304af5487c839b0
Miranda Honkanen <miranda.honkanen@helsinki.fi>
2023-12-24 21:07:13 +0200

    created database schema

---
+++ schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Pieces(
+    id SERIAL PRIMARY KEY,
+    title TEXT,
+    included BOOLEAN
+);
+
+CREATE TABLE Links(
+    id SERIAL PRIMARY KEY,
+    piece_id INTEGER REFERENCES Pieces(id),
+    link TEXT
+);
